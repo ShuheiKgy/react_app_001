ad5cd640d69aaad3274bc9cbd10bb1c3c6250265
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-07-31 16:57:33 +0900

    初期状態に戻した

--- src/app.js
+++ src/app.js
@@ -1,19 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import styles from "./css/app.css"
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-export default class App extends React.Component {
+class App extends React.Component {
     render() {
-        return (
-            <div id="main">
-                <h1 className="title">{"Hello World"}</h1>
-            </div>
-        )
+        return <h1>Hello, World</h1>;
     }
 }
 
 ReactDOM.render(
     <App />,
-    document.getElementById("app")
+    document.getElementById('app')
 )
-
